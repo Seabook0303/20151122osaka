6bd0b5231b702c558fd97bec9ed629337ac61040
Seabook0303 <k.t.seabook0303@gmail.com>
2015-11-19 20:51:15 +0900

    add point
    add point test1

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,10 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+var point = viewer.entities;
+
+point.add({
+        position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.YELLOW
+    }
+});
