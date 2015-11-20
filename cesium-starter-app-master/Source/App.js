3abf9d0a265e0e0a640115d1011c9c50a43001a7
Seabook0303 <k.t.seabook0303@gmail.com>
2015-11-20 13:08:05 +0900

    add point
    add point in Osaka

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,39 +1,10 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
-
 var point = viewer.entities;
 
 point.add({
-		position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
-		point : {
-				pixelSize : 10,
-				color : Cesium.Color.YELLOW
-	}
-});
-
-var line1 = viewer.entities;
-
-line1.add({
-	name : 'Red line on the surface',
-	polyline : {
-		positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
-														-125, 35]),
-		width : 5,
-		material : Cesium.Color.RED
-	}
-});
-
-var Polygon = viewer.entities;
-
-Polygon.add({
-    name : 'Polygon',
-    polygon : {
-        hierarchy : Cesium.Cartesian3.fromDegreesArray([-108.0, 42.0,
-                                                        -100.0, 42.0,
-                                                        -104.0, 40.0]),
-        extrudedHeight: 500000.0,
-	material : Cesium.Color.GREEN
+        position : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.YELLOW
     }
 });
-
-
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
