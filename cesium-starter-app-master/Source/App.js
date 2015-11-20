92095df975199c5d186e129d3ce30ddf6d405e45
Seabook0303 <k.t.seabook0303@gmail.com>
2015-11-20 13:17:34 +0900

    add line
    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -8,3 +8,16 @@ point.add({
             color : Cesium.Color.YELLOW
     }
 });
+
+
+var line1 = viewer.entities;
+
+line1.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([135.502101, 34.693684,
+                                                                                  139.692130, 35.689667]),
+        width : 5,
+        material : Cesium.Color.RED
+    }
+});
