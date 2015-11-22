2695f1475160cc78e39c1d445e42cdc9d4c455fb
Seabook0303 <k.t.seabook0303@gmail.com>
2015-11-22 14:04:05 +0900

    test
    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/
+20151122osaka/cesium-starter-app-master/foss4gosakakml/foss4gosaka.kml'));
