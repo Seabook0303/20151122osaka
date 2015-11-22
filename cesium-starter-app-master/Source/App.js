72c125b837d58fb918387ffea4aca4c3f95a69ea
Seabook0303 <k.t.seabook0303@gmail.com>
2015-11-22 14:08:04 +0900

    test
    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,9 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/
-20151122osaka/cesium-starter-app-master/foss4gosakakml/foss4gosaka.kml'));
+var point = viewer.entities;
+point.add({
+position : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684),
+point : {
+pixelSize : 10,
+color : Cesium.Color.YELLOW
+}
+});
